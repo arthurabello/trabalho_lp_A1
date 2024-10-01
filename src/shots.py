93f59c24095bfdec899f9b08d9f79d1367e2c154
rodrisevero <rodrigo.severo2003@gmail.com>
2024-10-01 20:03:59 -0300

    new changes
    alterando dicionario para dataframe
    
    adicionando documentação de map_column_values

--- src/shots.py
+++ src/shots.py
@@ -1,8 +1,9 @@
 import pandas as pd
-from typing import List, Dict, Union
+import doctest
+from typing import List, Tuple, Dict, Union
 
 def remove_columns(df: pd.DataFrame, columns: List[str]) -> pd.DataFrame:
-    """Função para remover colunas de um DataFrame
+    """Remove colunas de um DataFrame
 
     Args:
         df (pd.DataFrame): DataFrame a ser recebido pela função
@@ -86,21 +87,42 @@ def remove_lines_by_condition(df: pd.DataFrame, column: str, conditions: List[Un
         df = df[df[column] != cond]
     return df
 
-def map_shot_outcomes(df: pd.DataFrame) -> pd.DataFrame:
-    shots_mapping = {
-        1.0: 'Gol', 
-        2.0: 'Fora',
-        3.0: 'Defendido',
-        4.0: 'Trave'
-    }
-    df['shot_outcome'] = df['shot_outcome'].map(shots_mapping)
+def map_column_values(df: pd.DataFrame, column: str, map: Dict) -> pd.DataFrame:
+    """Mapeia os valores de uma coluna específica de acordo com um mapeamento
+    dado por meio de um dicionário.
+
+    Args:
+        df (pd.DataFrame): DataFrame a ser recebido pela função.
+        column (str): Nome da coluna que será mapeada.
+        map (Dict): Dicionário de mapeamento dos valores.
+
+    Returns:
+        pd.DataFrame: Dataframe com a coluna mapeada.
+    
+    Examples:
+        >>> data = [
+        ...        [1, 'Arnaldo', 7.0], 
+        ...        [2, 'Bernaldo', 8.5], 
+        ...        [3, 'Cernaldo', 7.0]
+        ...        ]
+        >>> df = pd.DataFrame(data, columns=['ID', 'Nome', 'Nota'])
+        >>> mapping = {'Arnaldo': 'Aluno 1', 'Bernaldo': 'Aluno 2', 'Cernaldo': 'Aluno 3'}
+        >>> map_column_values(df, 'Nome', mapping)
+           ID     Nome  Nota
+        0   1  Aluno 1   7.0
+        1   2  Aluno 2   8.5
+        2   3  Aluno 3   7.0"""
+    
+    df[column] = df[column].map(map)
+
     return df
 
 
-def calculate_goals(goals: pd.DataFrame) -> tuple:
-    total_goals = len(goals)
-    goals_inside = len(goals[goals['situation'] == 'inside'])
-    goals_outside = len(goals[goals['situation'] == 'outside'])
+def calculate_goals(goals: pd.DataFrame) -> Tuple:
+
+    total_goals = goals.shape[0]
+    goals_inside = filter_df(goals, 'situation', 'inside').shape[0]
+    goals_outside = filter_df(goals, 'situation', 'outside').shape[0]
 
     perc_inside = (goals_inside / total_goals) * 100
     perc_outside = (goals_outside / total_goals) * 100
@@ -109,27 +131,24 @@ def calculate_goals(goals: pd.DataFrame) -> tuple:
 
 
 def print_goals(perc_inside: float, perc_outside: float) -> None:
-
     print(f'{" ESTATÍSTICAS POR GOLS ":=^40}')
     print('-'*40)
     print(f'Dentro da área | {perc_inside:.2f}%')
     print(f'Fora da área   | {perc_outside:.2f}%')
     print('-'*40)
 
+# Usando dataframe
+def shot_outcome_by_situation(df: pd.DataFrame, situation: str) -> pd.DataFrame:
+    attempts = filter_df(df, 'situation', situation)['shot_outcome'].value_counts().reset_index()
+    attempts.columns = ['Resultado', 'count']
+    return attempts
 
-def shot_outcome_by_situation(df: pd.DataFrame, situation: str) -> Dict:
-    attempts = df[df['situation'] == situation]
-    return attempts['shot_outcome'].value_counts()
-
-def perc_attempts(df: pd.DataFrame, situation: str) -> Dict:
+def perc_attempts(df: pd.DataFrame, situation: str) -> pd.DataFrame:
     attempts = shot_outcome_by_situation(df, situation)
-    sum_attempts = sum(attempts)
-    perc_attempts = attempts / sum_attempts
-    return perc_attempts
-
-def print_dict(attempts: Dict) -> None:
-    for key, value in attempts.items():
-        print(f"{key:<10} | {value:.2%}")
+    attempts['Porcentagem'] = (attempts['count'] / attempts['count'].sum()) * 100
+    attempts['Porcentagem'] = attempts['Porcentagem'].round(2)
+    remove_columns(attempts, ['count'])
+    return attempts
 
 def print_shot_outcome(df: pd.DataFrame) -> None:
 
@@ -139,11 +158,11 @@ def print_shot_outcome(df: pd.DataFrame) -> None:
     print(f'{" ESTATÍSTICAS POR CHUTE ":=^40}')
     print('-'*40)
     print(f'Dentro da área: ')
-    print_dict(perc_inside_attempts)
+    print(perc_inside_attempts)
 
     print('-'*40)
     print(f'Fora da área: ')
-    print_dict(perc_outside_attempts)
+    print(perc_outside_attempts)
     print('-'*40)
 
 def main():
@@ -151,41 +170,24 @@ def main():
     filepath = "../data/cleaned_events.csv"
     df = pd.read_csv(filepath)
 
-    # Removendo colunas que não acrescentam na analise
-    remove_columns(df, ['time', 'side', 'bodypart'])
-
-    # Iremos verificar apenas Attempts 
-    filter_df(df, 'event_type', 1)
-
-    # retirar não registrados (19) e inuteis (1 e 2)
-    # 7 e 8 angulo dificil a direita ou a esquerda nao da informacao se é dentro
-    # ou fora da area, entao sera mais vantajoso remover
-    remove_lines_by_condition(df, 'location', [1, 2, 7, 8, 19])
-
-    # Agora, essa coluna não será mais necessaria
-
-    remove_columns(df, ['event_type'])
+    df = remove_columns(df, ['time', 'side', 'bodypart'])
+    df = filter_df(df, 'event_type', 1)
+    df = remove_lines_by_condition(df, 'location', [1, 2, 7, 8, 19])
+    df = remove_columns(df, ['event_type'])
 
     locations_inside = [3, 9, 10, 11, 12, 13, 14]
     df['situation'] = df['location'].apply(lambda x: 'inside' if x in locations_inside else 'outside')
 
-    # agora não precisaremos mais da linha de location
+    df = remove_columns(df, ['location'])
 
-    remove_columns(df, ['location'])
-
-    # Verificando o que foi gol e o que não foi e a relação com inside e outside
-    # Dos gols, vendo quantos são insides e quantos outsides
-
-    map_shot_outcomes(df)
+    shots_mapping = {1.0: 'Gol', 2.0: 'Fora',3.0: 'Defendido', 4.0: 'Trave'}
+    df = map_column_values(df, 'shot_outcome', shots_mapping)
     
     goals = filter_df(df,'is_goal', 1)
-
     perc_inside, perc_outside = calculate_goals(goals)
 
-    # Imprimir porcentagens de gols
     print_goals(perc_inside, perc_outside)
-
-    # Calculando porcentagens de outcomes fora e dentro da área
+    
     print_shot_outcome(df)
 
 if __name__ == '__main__':

744b77d7347be880aef1a30c88ee7121a4696bc9
rodrisevero <rodrigo.severo2003@gmail.com>
2024-10-03 17:04:12 -0300

    add test_utils
    alterando clean_data para usar o modulo  utils

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,21 +1,20 @@
 import pandas as pd
+from utils import remove_columns, remove_lines_by_condition
 
 filepath = "../data/events.csv"
 df = pd.read_csv(filepath)
 
 # Removendo colunas desnecessárias
-remove_colums = ['id_odsp', 'id_event','sort_order','text','event_type2','event_team',
-                 'opponent', 'player', 'player2','player_in','player_out', 'shot_place'
-                ,'shot_outcome', 'assist_method', 'situation', 'fast_break']
+columns_to_remove = ['id_event','sort_order','text','event_type2','event_team',
+                    'opponent', 'player', 'player2','player_in','player_out', 'shot_place'
+                    ,'assist_method', 'situation', 'fast_break']
 
-for column in remove_colums:
-    df.drop(column, axis=1, inplace=True)
+remove_columns(df, columns_to_remove)
 
 # Removendo linhas com eventos desnecessários
 # [Announcement, yellow card, second yellow card, red card, substitution, offside, hand ball]
-remove_events = [0, 4, 5, 6, 7, 8, 10]
-for events in remove_events: 
-    df = df[df['event_type'] != events]
+events_to_remove = [0, 4, 5, 6, 7, 8, 10]
+remove_lines_by_condition(df, 'event_type', events_to_remove)
 
 new_filepath = "../data/cleaned_events.csv"
 df.to_csv(new_filepath, index=False)

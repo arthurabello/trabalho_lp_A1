a5cbb4bc62b7374fa7fa9c88c57cdd6bcdb02cf8
Rodrigo Severo Araújo <160537806+rodrisevero@users.noreply.github.com>
2024-10-08 14:37:37 -0300

    Adicionando Raises para utils.py (#7)
    * adding raises

--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,10 @@ def remove_columns(df: pd.DataFrame, columns: List[str]) -> pd.DataFrame:
 
     Returns:
         pd.DataFrame: Retorna o DataFrame com as colunas deletadas
+
+    Raises:
+        TypeError: Se `df` não for um pd.DataFrame ou `columns` não for uma lista.
+        KeyError: Se alguma coluna em `columns` não existir no DataFrame.
         
     Examples:
         >>> data = [
@@ -24,9 +28,21 @@ def remove_columns(df: pd.DataFrame, columns: List[str]) -> pd.DataFrame:
         1   8.5
         2   9.7
     """
+    # raises
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError("O parâmetro 'df' deve ser um pandas DataFrame.")
+    
+    if not isinstance(columns, List):
+        raise TypeError("O parâmetro 'columns' deve ser uma lista")
+    
+    missing_columns = set(columns) - set(df.columns)
+    if missing_columns:
+        raise KeyError(f"As seguintes colunas não existem no DataFrame: {missing_columns}")
+
+    # main code
     for column in columns:
         df.drop(column, axis=1, inplace=True)
- 
+
     return df
 
 
@@ -41,6 +57,10 @@ def filter_df(df: pd.DataFrame, conditions: Dict[str, Union[str, int, float]]) -
     Returns:
         pd.DataFrame: DataFrame filtrado contendo apenas as linhas que atendem as soluções.
 
+    Raises:
+        TypeError: Se `df` não for um pd.DataFrame ou `conditions` não for um dicionário.
+        KeyError: Se alguma coluna em `conditions` não existir no DataFrame.
+
     Examples:
         >>> data = [
         ...        [1, 'Arnaldo', 7.0], 
@@ -53,6 +73,19 @@ def filter_df(df: pd.DataFrame, conditions: Dict[str, Union[str, int, float]]) -
         0   1   Arnaldo   7.0
         2   3  Cernaldo   7.0
     """
+    # raises
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError("O parâmetro 'df' deve ser um pandas DataFrame.")
+    
+    if not isinstance(conditions, Dict):
+        raise TypeError("O parâmetro 'conditions' deve ser um dicionário")
+    
+    for column in conditions:
+        if column not in df.columns:
+            raise KeyError(f"A coluna '{column}' não existe no DataFrame.")
+
+    # main code
+
     for column, value in conditions.items():
         df = df[df[column] == value]
     
@@ -71,6 +104,10 @@ def remove_lines_by_condition(df: pd.DataFrame, column: str, conditions: List[Un
     Returns:
         pd.DataFrame: DataFrame filtrado sem as linhas que atendem às condições.
 
+    Raises:
+        TypeError: Se `df` não for um pd.DataFrame ou `conditions` não for uma lista.
+        KeyError: Se a coluna 'column' não existir no DataFrame.
+
     Examples:
         >>> data = [
         ...        [1, 'Arnaldo', 7.0], 
@@ -83,6 +120,18 @@ def remove_lines_by_condition(df: pd.DataFrame, column: str, conditions: List[Un
         1   2  Bernaldo   8.5
 
     """
+    
+    # raises
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError("O parâmetro 'df' deve ser um pandas DataFrame.")
+
+    if not isinstance(conditions, List):
+        raise TypeError("O parâmetro 'conditions' deve ser uma lista.")
+    
+    if column not in df.columns:
+        raise KeyError(f"A coluna '{column}' não existe no DataFrame.")
+    
+    # main code
     for cond in conditions:
         df = df[df[column] != cond]
     return df
@@ -99,6 +148,10 @@ def map_column_values(df: pd.DataFrame, column: str, map: Dict) -> pd.DataFrame:
     Returns:
         pd.DataFrame: Dataframe com a coluna mapeada.
     
+    Raises:
+        TypeError: Se `df` não for um pd.DataFrame ou `map` não for um dicionário.
+        KeyError: Se a coluna 'column' não existir no DataFrame.
+
     Examples:
         >>> data = [
         ...        [1, 'Arnaldo', 7.0], 
@@ -112,7 +165,19 @@ def map_column_values(df: pd.DataFrame, column: str, map: Dict) -> pd.DataFrame:
         0   1  Aluno 1   7.0
         1   2  Aluno 2   8.5
         2   3  Aluno 3   7.0"""
+
+    # raises
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError("O parâmetro 'df' deve ser um pandas DataFrame.")
+
+    if not isinstance(map, Dict):
+        raise TypeError("O parâmetro 'map' deve ser um dicionário.")
     
+    if column not in df.columns:
+        raise KeyError(f"A coluna '{column}' não existe no DataFrame.")
+    
+    # main code
+
     df[column] = df[column].map(map)
 
     return df
@@ -123,7 +188,18 @@ def print_dataframe(df: pd.DataFrame, title: str) -> None:
     Args:
         df (pd.DataFrame): O DataFrame a ser exibido.
         titulo (str): O título a ser exibido acima do DataFrame.
+
+    Raises:
+        TypeError: Se `df` não for um pd.DataFrame ou `title` não for uma string.
     """
+    # raises
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError("O parâmetro 'df' deve ser um pandas DataFrame.")
+    
+    if not isinstance(title, str):
+        raise TypeError("O parâmetro 'title' deve ser uma string.")
+    
+    # main code
     print('-'*50)
     print(f'{f"  {title}  ":=^50}')
     print('-'*50)

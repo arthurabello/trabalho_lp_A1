ff2fd1d4f2413bbf99fc4e9c87c9e72c91e90aef
theBatista <filho.antonio@fgv.edu.br>
2024-10-08 15:09:59 -0300

    headed goals hypothesis calculations completed

--- src/head_part.py
+++ src/head_part.py
@@ -27,6 +27,78 @@ def get_rows_with_previous(df: pd.DataFrame, conditions: Dict[str, Union[str, in
 
     return df.iloc[indices_to_save].reset_index(drop=True)
 
+def is_headed_goal(df: pd.DataFrame, row_index: int) -> bool:
+    """Confere se um evento é um gol de cabeça.
+
+    Args:
+        df (pd.DataFrame): Dataframe que contém os eventos.
+        row_index (int): Índice da linha do evento a ser conferido.
+
+    Returns:
+        bool: True se refere-se a um gol de cabeça, e False caso contrário.
+    """
+    if df.loc[row_index, 'is_goal'] == 1 and df.loc[row_index, 'bodypart'] == 3:
+        return True
+    else:
+        return False
+
+def is_same_match(df: pd.DataFrame, row_index_a: int, row_index_b: int) -> bool:
+    """Confere se dois eventos ocorreram no mesmo jogo.
+
+    Args:
+        df (pd.DataFrame): Dataframe que contém os eventos.
+        row_index_a (int): Índice da linha do primeiro evento.
+        row_index_b (int): Índice da linha do segundo evento.
+
+    Returns:
+        bool: True se os eventos ocorreram no mesmo jogo, e False caso contrário.
+    """
+    if df.loc[row_index_a, 'id_odsp'] == df.loc[row_index_b, 'id_odsp']:
+        return True
+    else:
+        return False
+
+def origin_of_headed_goals(df: pd.DataFrame) -> pd.DataFrame:
+    """Calcula a porcentagem das origens dos gols de cabeça.
+
+    Args:
+        df (pd.DataFrame): Dataframe que contém os gols de cabeça e os eventos
+        anteriores.
+
+    Returns:
+        pd.DataFrame: DataFrame contendo as seguintes colunas:
+                      - 'Origem': O tipo do evento anterior ao gol de cabeça.
+                      - 'Porcentagem': A porcentagem referente a cada origem.
+    """
+    corners = 0
+    fouls = 0
+    offsides = 0
+    others = 0
+    for i in range(df.shape[0]):
+        if (is_headed_goal(df, i) and is_same_match(df, i, i-1) and
+            (df.loc[i, 'time'] - df.loc[i-1, 'time']) <= 1):
+
+            if df.loc[i-1, 'event_type'] == 2:
+                corners += 1
+            elif df.loc[i-1, 'event_type'] == 3:
+                fouls += 1
+            elif df.loc[i-1, 'event_type'] == 9:
+                offsides += 1
+            else:
+                others += 1
+
+    total = corners + fouls + offsides + others
+
+    results = pd.DataFrame({
+    'Origem': ['Escanteios', 'Faltas', 'Impedimentos', 'Outros'],
+    'Porcentagem': [round((corners / total) * 100, 2), 
+                    round((fouls / total) * 100, 2),
+                    round((offsides / total) * 100, 2), 
+                    round((others / total) * 100, 2)]
+})
+
+    return results
+
 def main():
     filepath = "../data/cleaned_events.csv"
     df = pd.read_csv(filepath)
@@ -34,9 +106,8 @@ def main():
     remove_columns(df, ['side', 'shot_outcome', 'location'])
     df = get_rows_with_previous(df, {'bodypart': 3, 'is_goal': 1})
 
-    dead_ball_types = [2, 3, 9]
-
-    print(df.head(50))
+    c = origin_of_headed_goals(df)
+    print(c)
 
 if __name__ == '__main__':
     main()
